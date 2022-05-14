bbd3c2c7bd61b30a0e184ea8b13d1a590fda88ca
Andrei <andrei.indosov@gmail.com>
2022-05-15 01:10:02 +0800

    typescript refactor

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+// data
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+
+
+    </main>
+  )
+}
+
+export default IndexPage
